bbaad92b65f9f2ee7c853dbcdb6cdbbd3a6dee3d
GustavoPrad <gustavoprado641@gmail.com>
2024-10-29 20:13:22 -0300

    Adicao de esqueci minha senha e autenticacao via google e github

--- main.py
+++ main.py
@@ -1,20 +1,79 @@
-from flask import Flask, request, render_template, flash, redirect, url_for
+from flask import Flask, request, render_template, flash, redirect, url_for, session
 import sqlite3
-from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash     #converte dados em senha, sem reversão
+from datetime import datetime, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_dance.contrib.github import make_github_blueprint, github
 
 # Configuração da aplicação Flask
 app = Flask(__name__)
-app.secret_key = 'chave_secreta'
+app.secret_key = 'peixefrito123'
+
+# Configuração para autenticação com o Google
+google_bp = make_google_blueprint(
+    client_id="SEU_GOOGLE_CLIENT_ID",
+    client_secret="SEU_GOOGLE_CLIENT_SECRET",
+    redirect_to="google_login"
+)
+app.register_blueprint(google_bp, url_prefix="/login")
+
+# Configuração para autenticação com o GitHub
+github_bp = make_github_blueprint(
+    client_id="Ov23liGunVmtbGYth4Rv",
+    client_secret="0496405b06a8dedbe843f27591c2fc7f86abf7c2",
+    redirect_to="github_login"
+)
+app.register_blueprint(github_bp, url_prefix="/login/github")
+
+# Rota para login com Google
+@app.route('/login/google')
+def google_login():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    user_info = resp.json()
+    session['google_user'] = user_info
+    flash(f"Logado com Google como {user_info['email']}", "success")
+    return redirect(url_for("loja"))
+
+# Rota para login com GitHub
+@app.route('/login/github')
+def github_login():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok, resp.text
+    user_info = resp.json()
+    session['github_user'] = user_info
+    flash(f"Logado com GitHub como {user_info['login']}", "success")
+    return redirect(url_for("loja"))
 
 # Configuração do banco de dados SQLite3
 DATABASE = 'database.db'
+
+# Configuração do servidor de e-mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'gustavoprado641@gmail.com'
+app.config['MAIL_PASSWORD'] = 'qlif zytt sukc pqwj'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+# Serializador para gerar os tokens seguros
+s = URLSafeTimedSerializer(app.secret_key)
 
 # Conectar ao BD
 def get_db():
     db = sqlite3.connect(DATABASE)
     db.row_factory = sqlite3.Row
     return db
+
 
 # Criar a tabela de dados, se ainda não existir
 def init_db():
@@ -24,6 +83,7 @@ def init_db():
             db.cursor().executescript(f.read())
             db.commit()
 
+
 # Rota para inicializar o banco de dados
 @app.route('/initdb')
 def initialize_database():
@@ -31,10 +91,12 @@ def initialize_database():
     flash('Banco de dados inicializado com sucesso', 'success')
     return redirect(url_for('home'))
 
+
 # Rota raiz para renderizar o template HTML
 @app.route('/')
 def home():
     return render_template('base.html')
+
 
 # Rota para login
 @app.route('/login', methods=['GET', 'POST'])
@@ -46,29 +108,80 @@ def login():
         conn = get_db()
         cursor = conn.cursor()
 
-        # Verificação de credencais
+        # Verificação de credenciais
         cursor.execute('SELECT * FROM usuarios WHERE login = ?', (login,))
         user = cursor.fetchone()
 
         if user and check_password_hash(user['senha'], senha):
-            # Verificação se usuário é ativo
             if user['status'] == 1:  # 1 é ativo, 2 é inativo
                 flash('Login bem-sucedido!', 'success')
-                return redirect(url_for('loja'))  # Redirecionar para a página loja.html
+                return redirect(url_for('loja'))
             else:
                 flash('Conta inativa.', 'danger')
-
         else:
             flash('Login ou senha inválidos.', 'danger')
 
         conn.close()
 
-    return render_template('login.html')  # Renderize a página de login
+    return render_template('login.html')
+
+# Rota para solicitar redefinição de senha
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        # Aqui vocês irão verificar no banco de dados se o email recebido é válido,
+        # ou seja, se é usuário do sistema.
+
+        # Preparação e envio do email
+        token = s.dumps(email, salt='password_recovery')
+        msg = Message('Redefinição de senha', sender='gustavoprado641@gmail.com', recipients=[email])
+        link = url_for('reset_password', token=token, _external=True)
+        msg.body = f'Clique no link para redefinir sua senha: {link}'
+        mail.send(msg)
+
+        flash('Um link de recuperação de senha foi enviado para o seu email', category='success')
+
+        return redirect(url_for('login'))
+
+    return render_template('forgot_password.html')
+
+# Rota para redefinir a senha
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    try:
+        # Carregar o e-mail do token
+        email = s.loads(token, salt='password_recovery', max_age=3600)  # 1 hora
+    except SignatureExpired:
+        return '<h1> O link de redefinição de senha expirou!!!</h1>'
+    except BadSignature:
+        return '<h1> Token inválido!!! </h1>'
+
+    if request.method == 'POST':
+        new_password = request.form['password']
+        hashed_password = generate_password_hash(new_password)  # Gerar hash da nova senha
+
+        # Conectar ao banco e atualizar a senha
+        conn = get_db()
+        cursor = conn.cursor()
+        cursor.execute(
+            'UPDATE usuarios SET senha = ?, reset_token = NULL, reset_token_expiry = NULL WHERE login = ?',
+            (hashed_password, email)
+        )
+        conn.commit()
+        conn.close()
+
+        flash('Sua senha foi redefinida com sucesso!', category='success')
+        return redirect(url_for('login'))
+
+    return render_template('reset_password.html', token=token)
+
 
 # Rota para loja
 @app.route('/loja')
 def loja():
     return render_template('loja.html')
+
 
 # Rota para cadastro de usuários
 @app.route('/cadastro', methods=['GET', 'POST'])
@@ -77,36 +190,34 @@ def cadastro():
         login = request.form['login']
         senha = generate_password_hash(request.form['senha'])
         nome_completo = request.form['nome_completo']
-        status = request.form['status']  # 1 para ativo ou 0 para inativo
+        status = request.form['status']
 
-        # Conexão ao banco de dados
         conn = get_db()
         cursor = conn.cursor()
 
-        # Verificando se o login já existe
         cursor.execute('SELECT * FROM usuarios WHERE login = ?', (login,))
         existing_user = cursor.fetchone()
 
         if existing_user:
             flash('Login já cadastrado. Tente outro.', 'danger')
         else:
-            # Inserindo novo usuário com a data de criação
             cursor.execute(
                 'INSERT INTO usuarios (login, senha, nome_completo, status, created, modified) VALUES (?, ?, ?, ?, ?, ?)',
-                (login, senha, nome_completo, status, datetime.now(), datetime.now()))
+                (login, senha, nome_completo, status, datetime.now(), datetime.now())
+            )
             conn.commit()
             flash('Usuário cadastrado com sucesso!', 'success')
 
         conn.close()
         return redirect(url_for('cadastro'))
 
-    # Tabela com todos usuários cadastrados
     conn = get_db()
     usuarios = conn.execute('SELECT * FROM usuarios').fetchall()
     conn.close()
     return render_template('cadastro.html', usuarios=usuarios)
 
-# Rota para edição de usuários (não exclui usuário, apenas muda de ativo p/ inativo)
+
+# Rota para edição de usuários
 @app.route('/editar_usuario/<int:id>', methods=['POST'])
 def editar_usuario(id):
     nome_completo = request.form['nome_completo']
@@ -114,8 +225,10 @@ def editar_usuario(id):
     status = request.form['status']
 
     conn = get_db()
-    conn.execute('UPDATE usuarios SET nome_completo = ?, login = ?, status = ?, modified = ? WHERE id = ?',
-                 (nome_completo, login, status, datetime.now(), id))
+    conn.execute(
+        'UPDATE usuarios SET nome_completo = ?, login = ?, status = ?, modified = ? WHERE id = ?',
+        (nome_completo, login, status, datetime.now(), id)
+    )
     conn.commit()
     conn.close()
 
